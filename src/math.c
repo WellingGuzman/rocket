551c38b4a1e403273f0871f6a42db92e2eb1e3ed
Welling Guzman <hola@wellingguzman.com>
2017-11-05 21:40:04 -0400

    add rgb to rgba

---
+++ src/math.c
@@ -0,0 +1,7 @@
+#include "math.h"
+
+u32
+rgb_to_rgba(const u32 c)
+{
+    return (c << 8) | 0xFF;
+}
---
+++ src/math.h
@@ -0,0 +1,9 @@
+#ifndef math_h
+#define math_h
+
+#include "types.h"
+
+u32
+rgb_to_rgba(const u32 c);
+
+#endif
