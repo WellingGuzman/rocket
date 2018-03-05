77048d0a231b32f672527c0666e4a42483006d54
Welling Guzman <hola@wellingguzman.com>
2018-03-04 21:59:09 -0400

    add color macros

--- src/colors.h
+++ src/colors.h
@@ -3,6 +3,11 @@
 
 #include "head.h"
 
+#define RED(color) ((color & 0xFF000000) >> 24)
+#define GREEN(color) ((color & 0x00FF0000) >> 16)
+#define BLUE(color) ((color & 0x0000FF00) >> 8)
+#define ALPHA(color) (color & 0x000000FF)
+
 u32
 rgb_to_rgba(const u32 c);
 
